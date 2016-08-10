54560cf83fe20e741ae9f7cdc90a7f9abd16cebb
fredokun <Frederic.Peschanski@lip6.fr>
2016-08-10 17:25:27 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject latte-sets "0.0.4-SNAPSHOT"
+(defproject latte-sets "0.0.5-SNAPSHOT"
   :description "A formalization of (typed) Set theory in LaTTe."
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [latte "0.2.1-SNAPSHOT"]]
+                 [latte "0.3.1-SNAPSHOT"]]
   :codox {:metadata {:doc/format :markdown}
           :namespaces [latte-sets.core]}
   :plugins [[lein-codox "0.9.5"]])
