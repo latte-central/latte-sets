0b90f4c8feca8cb12d820668309e849486585f32
Fredokun <fredokun@github.com>
2016-11-08 15:29:10 +0100

    implicit discharges everywhere

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject latte-sets "0.0.7-SNAPSHOT"
+(defproject latte-sets "0.1.0-SNAPSHOT"
   :description "A formalization of (typed) Set theory in LaTTe."
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [latte "0.3.6-SNAPSHOT"]]
+                 [latte "0.4.0-SNAPSHOT"]]
   :codox {:output-path "docs"
           :metadata {:doc/format :markdown}
           :namespaces [latte-sets.core
                        latte-sets.powerset]}
-  :plugins [[lein-codox "0.10.0"]])
+  :plugins [[lein-codox "0.10.1"]])
