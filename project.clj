6dd885132f19457499568eabf03d71e1a3f8a8d9
fredokun <frederic.peschanski@lip6.fr>
2019-01-03 13:10:08 +0100

    1.0b2 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject latte-sets "0.7.0-SNAPSHOT"
+(defproject latte-sets "1.0b2-SNAPSHOT"
   :description "A formalization of (typed) Set theory in LaTTe."
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [latte-prelude "0.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [latte-prelude "1.0b2-SNAPSHOT"]]
   :main latte-sets.main
   :aliases {"certify" ["run" ":certify"]
             "clear-cert" ["run" ":clear-cert"]}
