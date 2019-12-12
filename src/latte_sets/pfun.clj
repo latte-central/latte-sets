a24d4b9696a0df13c02a509eb1bd1d5b9f7f8de3
Frederic Peschanski <fredokun@github.com>
2019-12-12 19:41:37 +0100

    partial functions ongoing

--- src/latte_sets/pfun.clj
+++ src/latte_sets/pfun.clj
@@ -28,21 +28,20 @@
 
 (definition pfun
   "A partial function `f` based on a relation together with
-a domain set `from` and a codomain set `to`. Note that the relation `f` on
+a restricted domain set `from`. Note that the relation `f` on
 outside `from` or `to` need not be a function."
-  [[?T ?U :type], f (rel T U), from (set T), to (set U)]
+  [[?T ?U :type], f (rel T U), from (set T)]
   (forall-in [x from]
-    (forall-in [y1 to]
-      (forall-in [y2 to]
-        (==> (f x y1)
-             (f x y2)
-             (equal y1 y2))))))
+    (forall [y1 y2 U]
+      (==> (f x y1)
+           (f x y2)
+           (equal y1 y2)))))
 
 (defn fetch-pfun-type [def-env ctx t]
   (latte.utils/decomposer
    (fn [t]
      (if (clojure.core/and (seq? t)
-                           (= (count t) 6)
+                           (= (count t) 4)
                            (= (first t) #'latte-sets.pfun/pfun-def))
        (into [] (rest t))
        (throw (ex-info "Not a partial function type." {:type t}))))
@@ -50,74 +49,119 @@ outside `from` or `to` need not be a function."
 
 (defthm ridentity-pfun
   "The identity relation is a partial function on
-any domain and range sets."
+any domain set."
   [T :type]
-  (forall [t u (set T)]
-    (pfun (rel/identity T) t u)))
+  (forall [from (set T)]
+    (pfun (rel/identity T) from)))
 
 (proof 'ridentity-pfun
   (pose rid := (rel/identity T))
-  (assume [t (set T)
-           u (set T)
-           x T Hx (elem x t)
-           y1 T Hy1 (elem y1 u)
-           y2 T Hy2 (elem y2 u)
+  (assume [from (set T)
+           x T Hx (elem x from)
+           y1 T y2 T
            Hid1 (rid x y1)
            Hid2 (rid x y2)]
     (have <a> (equal y1 y2) :by (eq/eq-trans (eq/eq-sym Hid1) Hid2)))
   (qed <a>))
 
+(definition pfun-fun
+  "The \"partial\" function of a (total) type-theoretic function `f` on its whole domain"
+  [[?T ?U :type] [f (==> T U)]]
+  (lambda [x T]
+    (lambda [y U]
+      (equal (f x) y))))
+
+(defthm pfun-fun-prop
+  "A type-theoretic function `f` is a partial function for any domain
+restriction."
+  [[?T ?U :type], f (==> T U)]
+  (forall [from (set T)]
+    (pfun (pfun-fun f) from)))
+
+(proof 'pfun-fun-prop-thm
+  (pose pf := (pfun-fun f))
+  (assume [from (set T)
+           x T Hx (elem x from)
+           y1 U y2 U
+           Hy1 (pf x y1)
+           Hy2 (pf x y2)]
+    (have <a> (equal y1 y2) :by (eq/eq-trans (eq/eq-sym Hy1) Hy2)))
+  (qed <a>))
+
 (definition pdom
-  "The domain of partial function `f`, taking antecedents in `from`
-and images in `to`."
-  [[?T ?U :type], f (rel T U), from (set T), to (set U)]
+  "The actual domain of partial function `f`, taking antecedents in `from`."
+  [[?T ?U :type], f (rel T U), from (set T)]
   (set-of [x T] (and (elem x from)
-                     (exists-in [y to] (f x y)))))
+                     (exists [y U] (f x y)))))
 
 (definition pran
-  "The range of partial function `f``, taking antecedents in `from`
-and images in `to`."
-  [[?T ?U :type], f (rel T U), from (set T), to (set U)]
-  (set-of [y U] (and (elem y to)
-                     (exists-in [x from] (f x y)))))
+  "The range of partial function `f``, taking antecedents in `from`."
+  [[?T ?U :type], f (rel T U), from (set T)]
+  (set-of [y U] (exists-in [x from] (f x y))))
 
 (definition ptotal
-  "The partial function `f` is total wrt. the provided `from`/`to` sets."
-  [[?T ?U :type], f (rel T U), from (set T), to (set U)]
-  (set-equal (pdom f from to) from))
+  "The partial function `f` is total wrt. the provided `from` domain set."
+  [[?T ?U :type], f (rel T U), from (set T)]
+  (set-equal (pdom f from) from))
 
 (defthm ptotal-domain
-  [[?T ?U :type], f (rel T U), from (set T), to (set U)]
-  (==> (ptotal f from to)
+  [[?T ?U :type], f (rel T U), from (set T)]
+  (==> (ptotal f from)
        (forall-in [x from]
-         (exists-in [y to]
+         (exists [y U]
            (f x y)))))
 
 (proof 'ptotal-domain-thm
-  (assume [Htot (ptotal f from to)]
+  (assume [Htot (ptotal f from)]
     (assume [x T
              Hx (elem x from)]
-      (have <a> (set-equal from (pdom f from to))
-            :by ((s/set-equal-sym (pdom f from to) from) Htot))
-      (have <b> (s/seteq from (pdom f from to))
-            :by ((s/set-equal-implies-seteq from (pdom f from to)) <a>))
-      (have <c> (elem x (pdom f from to))
+      (have <a> (set-equal from (pdom f from))
+            :by ((s/set-equal-sym (pdom f from) from) Htot))
+      (have <b> (s/seteq from (pdom f from))
+            :by ((s/set-equal-implies-seteq from (pdom f from)) <a>))
+      (have <c> (elem x (pdom f from))
             :by (((p/and-elim-left <b>) x) Hx))
-      (have <d> (exists-in [y to] (f x y))
+      (have <d> (exists [y U] (f x y))
             :by (p/and-elim-right <c>))))
   (qed <d>))
 
+(defthm pfun-fun-total
+  "A type-theoretic function `f` is always total."
+  [[?T ?U :type], f (==> T U)]
+  (forall [from (set T)]
+    (ptotal (pfun-fun f) from)))
+
+(proof 'pfun-fun-total-thm
+  (pose rf := (pfun-fun f))
+  (assume [from (set T)]
+    "subset (if) part"
+    (assume [x T
+             Hx (elem x (pdom rf from))]
+      (have <a> (elem x from) :by (p/and-elim-left Hx)))
+    "cosubset (only if) part"
+    (assume [x T
+             Hx (elem x from)]
+      (have <b1> (rf x (f x)) :by (eq/eq-refl (f x)))
+      (have <b2> (exists [y U] (rf x y))
+            :by ((q/ex-intro (lambda [y U] (rf x y)) (f x)) <b1>))
+      (have <b> (elem x (pdom rf from)) :by (p/and-intro Hx <b2>)))
+    "(sub)set equality"
+    (have <c> (s/seteq (pdom rf from) from) :by (p/and-intro <a> <b>))
+    "to set equality"
+    (have <d> (set-equal (pdom rf from) from)
+          :by ((s/seteq-implies-set-equal (pdom rf from) from)
+               <c>)))
+  (qed <d>))
+
 (definition pcompose
-  [[?T ?U ?V :type], f (rel U V), ffrom (set U), fto (set V), g (rel T U), gfrom (set T), gto (set U)]
+  [[?T ?U ?V :type], f (rel U V), ffrom (set U), g (rel T U), gfrom (set T)]
   (lambda [x T]
     (lambda [z V]
       (==> (elem x gfrom)
-           (elem z fto)
            (exists [y U]
-             (and* (elem y gto)
-                  (elem y ffrom)
-                  (g x y)
-                  (f y z)))))))
+             (and* (elem y ffrom)
+                   (g x y)
+                   (f y z)))))))
 
 (defthm pcompose-pfun
   "The composition of two partial functions `f` and `g`."
