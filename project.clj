2052ee1f61063da734cf763b70fc4127293af269
fredokun <fredokun@github.com>
2025-03-13 13:23:20 +0100

    versions bump

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/fredokun/latte-sets.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [latte-prelude "1.0b10-SNAPSHOT"]]
   :main latte-sets.main
   :aliases {"certify" ["run" ":certify"]
